a467ffd095e43b1dfe6735793d5849666c1543d2
addetz <adelinasimion@gmail.com>
2022-02-05 20:34:51 +0000

    add tempF method to city

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 type city struct {
-	name string
-	temp float64
+	name  string
+	tempC float64 // temperature in Celsius
 }
 
 func newCity(n string) city {
@@ -13,9 +13,19 @@ func newCity(n string) city {
 	}
 }
 
+// Function equivalent to the tempF method.
+// func tempF(c city) float64 {
+// 	return (c.tempC * 9 / 5) + 32
+// }
+
+func (c city) tempF() float64 {
+	return (c.tempC * 9 / 5) + 32
+}
+
 func main() {
 	c := newCity("London")
-	c.temp = 7.5
+	c.tempC = 7.5
+	tempF := c.tempF()
 
-	fmt.Println("City:", c)
+	fmt.Printf("[%v]:tempC=%v;tempF=%v\n", c.name, c.tempC, tempF)
 }
