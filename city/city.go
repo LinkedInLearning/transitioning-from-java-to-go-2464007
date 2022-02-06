3a17cd559fc7e2f40ff831b20bb3aee3d3584e45
addetz <adelinasimion@gmail.com>
2022-02-06 21:40:58 +0000

    add modules to temp service

---
+++ city/city.go
@@ -0,0 +1,18 @@
+package city
+
+type City struct {
+	Name  string
+	TempC float64 // temperature in Celsius
+}
+
+//New creates a new City instance with the given name
+func New(n string) *City {
+	return &City{
+		Name: n,
+	}
+}
+
+//TempF returns the city temperature converted to Fahrenheit
+func (c City) TempF() float64 {
+	return (c.TempC * 9 / 5) + 32
+}
