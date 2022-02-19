75a017c5099f29e65f8657e1cc527cd8b403c748
addetz <adelinasimion@gmail.com>
2022-02-19 12:21:47 +0000

    set first challenge

--- models/city.go
+++ models/city.go
@@ -1,12 +1,31 @@
 package models
 
-type City struct {
-	Name  string
-	TempC float64 // temperature in Celsius
+var (
+	// hot enough for the beach
+	beachVacationThreshold = 22
+	// cold enough to snow
+	skiVacationThreshold = -2
+)
+
+type city struct {
+	name  string
+	tempC float64 // temperature in Celsius
 }
 
-// NewCity creates a new city instance with the given name and Celsius temperated
-func NewCity(n string, t float64) CityTemp {
+// CityTemp interface defines all things city temperature related
+type CityTemp interface {
+	Name() string
+	TempC() float64
+	TempF() float64
+
+	// IMPLEMENT ME!
+	SkiVacationReady() bool
+	BeachVacationReady() bool
+}
+
+//New creates a new city instance with the given name and Celsius temperated
+func NewCity(n string, t float64, hasBeach bool, hasMountain bool) CityTemp {
+	// MAKE ME COMPILE!
 	return &city{
 		name:  n,
 		tempC: t,
