bcb305f76bbbe307abbb4b39a6b83acb581f117d
addetz <adelinasimion@gmail.com>
2022-02-26 18:44:38 +0000

    add arrays example

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func addCity(city string, cities [2]string) {
+	cities[1] = city
+}
+
+func main() {
+	cities := [2]string{"London"}
+	copy := cities
+
+	cities[1] = "New York"
+	addCity("Miami", copy)
+	
+	fmt.Println("Cities:", cities)
+	fmt.Println("Copy:", copy)
+}
