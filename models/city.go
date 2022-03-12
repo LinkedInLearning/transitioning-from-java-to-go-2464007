897d10169149d47c06a09c993506ea9db84419a6
addetz <adelinasimion@gmail.com>
2022-03-12 15:37:22 +0000

    add challenge

--- models/city.go
+++ models/city.go
@@ -9,7 +9,7 @@ var (
 
 type city struct {
 	name        string
-	tempC       float64 // temperature in Celsius
+	tempC       []float64 // temperature in Celsius
 	hasBeach    bool
 	hasMountain bool
 }
@@ -17,14 +17,14 @@ type city struct {
 // CityTemp interface defines all things city temperature related
 type CityTemp interface {
 	Name() string
-	TempC() float64
-	TempF() float64
-	BeachVacationReady() bool
-	SkiVacationReady() bool
+	TempC(q CityQuery) float64
+	TempF(q CityQuery) float64
+	SkiVacationReady(q CityQuery) bool
+	BeachVacationReady(q CityQuery) bool
 }
 
-// NewCity creates a new city instance with the given name and Celsius temperated
-func NewCity(n string, t float64, hasBeach bool, hasMountain bool) CityTemp {
+// New creates a new city instance with the given name and Celsius temperated
+func NewCity(n string, t []float64, hasBeach bool, hasMountain bool) CityTemp {
 	return &city{
 		name:        n,
 		tempC:       t,
@@ -39,21 +39,21 @@ func (c city) Name() string {
 }
 
 // TempC returns the city temperature in Celsius
-func (c city) TempC() float64 {
-	return c.tempC
+func (c city) TempC(q CityQuery) float64 {
+	return c.tempC[q.Month()-1]
 }
 
 // TempF returns the city temperature converted to Fahrenheit
-func (c city) TempF() float64 {
-	return (c.tempC * 9 / 5) + 32
+func (c city) TempF(q CityQuery) float64 {
+	return (c.tempC[q.Month()-1] * 9 / 5) + 32
 }
 
 // BeachVacationReady returns whether the given city is suitable for a beach vacation.
-func (c city) BeachVacationReady() bool {
-	return c.hasBeach && c.tempC > beachVacationThreshold
+func (c city) BeachVacationReady(q CityQuery) bool {
+	return c.hasBeach && c.tempC[q.Month()-1] > beachVacationThreshold
 }
 
 // SkiVacationReady returns whether the given city is suitable for a ski vacation.
-func (c city) SkiVacationReady() bool {
-	return c.hasMountain && c.tempC < skiVacationThreshold
+func (c city) SkiVacationReady(q CityQuery) bool {
+	return c.hasMountain && c.tempC[q.Month()-1] < skiVacationThreshold
 }
