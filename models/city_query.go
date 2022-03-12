501adf05b5a0262409c582d952a60f854ed28e2f
addetz <adelinasimion@gmail.com>
2022-03-12 15:37:22 +0000

    add challenge solution

--- models/city_query.go
+++ models/city_query.go
@@ -20,7 +20,45 @@ type CityQuery interface {
 
 // NewQuery creates a new city query object, if the supplied values are valid
 func NewQuery(beach bool, ski bool, month int, name string) (CityQuery, error) {
-	// implement me
-	q := &query{}
-	return q, fmt.Errorf("NewQuery not implemented")
+	cq := &query{
+		beach: beach,
+		ski:   ski,
+		month: month,
+		name:  name,
+	}
+	if err := cq.validate(); err != nil {
+		return nil, err
+	}
+
+	return cq, nil
+}
+
+// validate runs all validations on the city query
+// month is a mandatory valid field now
+func (q query) validate() error {
+	if q.month < 1 || q.month > 12 {
+		return fmt.Errorf("month must be in the range [1,12]; got %v", q.month)
+	}
+
+	return nil
+}
+
+// Beach returns the query beach value
+func (q query) Beach() bool {
+	return q.beach
+}
+
+// Ski returns the query ski value
+func (q query) Ski() bool {
+	return q.ski
+}
+
+// Month returns the query month value
+func (q query) Month() int {
+	return q.month
+}
+
+// Name returns the query name value
+func (q query) Name() string {
+	return q.name
 }
--- printer/printer.go
+++ printer/printer.go
@@ -22,12 +22,12 @@ func New() *Printer {
 
 // CityHeader prints out the table header for the city table
 func (p *Printer) CityHeader() {
-	fmt.Fprintln(p.w, "Name\tTempC\tTempF\tBeach vacation ready?\tSki vacation ready?")
+	fmt.Fprintln(p.w, "Name\tTempC\tTempF\tBeach ready?\tSki ready?")
 }
 
 // CityDetails prints out the given city
-func (p *Printer) CityDetails(c models.CityTemp) {
-	fmt.Fprintf(p.w, "%v\t%v\t%v\t%v\t%v\n", c.Name(), c.TempC(), c.TempF(), c.BeachVacationReady(), c.SkiVacationReady())
+func (p *Printer) CityDetails(c models.CityTemp, cq models.CityQuery) {
+	fmt.Fprintf(p.w, "%v\t%v\t%v\t%v\t%v\n", c.Name(), c.TempC(cq), c.TempF(cq), c.BeachVacationReady(cq), c.SkiVacationReady(cq))
 }
 
 // Cleanup closes up the printer instance
