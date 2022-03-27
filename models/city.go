7b6f4e0c4e48aea3d9dac6c1ea9ead6e0ba8783b
addetz <adelinasimion@gmail.com>
2022-03-27 16:14:08 +0100

    add challenge solution

--- models/city.go
+++ models/city.go
@@ -2,14 +2,16 @@ package models
 
 var (
 	// hot enough for the beach
-	beachVacationThreshold = 22
+	beachVacationThreshold float64 = 22
 	// cold enough to snow
-	skiVacationThreshold = -2
+	skiVacationThreshold float64 = -2
 )
 
 type city struct {
-	name  string
-	tempC float64 // temperature in Celsius
+	name        string
+	tempC       float64 // temperature in Celsius
+	hasBeach    bool
+	hasMountain bool
 }
 
 // CityTemp interface defines all things city temperature related
@@ -17,22 +19,41 @@ type CityTemp interface {
 	Name() string
 	TempC() float64
 	TempF() float64
-
-	// IMPLEMENT ME!
-	SkiVacationReady() bool
 	BeachVacationReady() bool
+	SkiVacationReady() bool
 }
 
-//New creates a new city instance with the given name and Celsius temperated
+// NewCity creates a new city instance with the given name and Celsius temperated
 func NewCity(n string, t float64, hasBeach bool, hasMountain bool) CityTemp {
-	// MAKE ME COMPILE!
 	return &city{
-		name:  n,
-		tempC: t,
+		name:        n,
+		tempC:       t,
+		hasBeach:    hasBeach,
+		hasMountain: hasMountain,
 	}
 }
 
-//TempF returns the city temperature converted to Fahrenheit
-func (c City) TempF() float64 {
-	return (c.TempC * 9 / 5) + 32
+// Name returns the city name
+func (c city) Name() string {
+	return c.name
+}
+
+// TempC returns the city temperature in Celsius
+func (c city) TempC() float64 {
+	return c.tempC
+}
+
+// TempF returns the city temperature converted to Fahrenheit
+func (c city) TempF() float64 {
+	return (c.tempC * 9 / 5) + 32
+}
+
+// BeachVacationReady returns whether the given city is suitable for a beach vacation.
+func (c city) BeachVacationReady() bool {
+	return c.hasBeach && c.tempC > beachVacationThreshold
+}
+
+// SkiVacationReady returns whether the given city is suitable for a ski vacation.
+func (c city) SkiVacationReady() bool {
+	return c.hasMountain && c.tempC < skiVacationThreshold
 }
