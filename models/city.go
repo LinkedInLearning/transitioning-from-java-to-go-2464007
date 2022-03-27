ac768648e1ca09c8b54e4b02491cad60b45198b0
addetz <adelinasimion@gmail.com>
2022-03-27 13:15:58 +0100

    rename package and module

--- models/city.go
+++ models/city.go
@@ -1,36 +1,18 @@
 package models
 
-type city struct {
-	name  string
-	tempC float64 // temperature in Celsius
+type City struct {
+	Name  string
+	TempC float64 // temperature in Celsius
 }
 
-// CityTemp interface defines all things city temperature related
-type CityTemp interface {
-	Name() string
-	TempC() float64
-	TempF() float64
-}
-
-// NewCity creates a new city instance with the given name and Celsius temperated
-func NewCity(n string, t float64) CityTemp {
-	return &city{
-		name:  n,
-		tempC: t,
+//New creates a new City instance with the given name
+func NewCity(n string) *City {
+	return &City{
+		Name: n,
 	}
 }
 
-// Name returns the city name
-func (c city) Name() string {
-	return c.name
-}
-
-// TempC returns the city temperature in Celsius
-func (c city) TempC() float64 {
-	return c.tempC
-}
-
-// TempF returns the city temperature converted to Fahrenheit
-func (c city) TempF() float64 {
-	return (c.tempC * 9 / 5) + 32
+//TempF returns the city temperature converted to Fahrenheit
+func (c City) TempF() float64 {
+	return (c.TempC * 9 / 5) + 32
 }
